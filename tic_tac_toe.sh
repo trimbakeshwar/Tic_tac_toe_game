bd6e63ef5e9f24fd14f7f4e3c099ffcffb7ca3b0
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-26 16:38:16 +0530

    Added:-to block user by computer when he is win

--- tic_tac_toe.sh
+++ tic_tac_toe.sh
@@ -1,40 +1,66 @@
-#!/bin/bash -x
+#!/bin/bash -x 
 echo "welcome to tic_tac_toe"
 declare -A bord
 ROW=3
 COLUMN=3
-function reseting(){
-for (( i=0; i<$ROW; i++ ))
-do
-	for (( j=0; j<$COLUMN; j++ ))
+countOfMoves=0
+computerPlayer="o"
+totalNumberOfMoves=$((ROW*COLUMN))
+function resetingBord(){
+	for (( i=0; i<$ROW; i++ ))
 	do
-		bord[$i,$j]=" "
+		for (( j=0; j<$COLUMN; j++ ))
+		do
+			bord[$i,$j]=" "
+		done
 	done
-done
 }
-reseting
-function toss(){
-	toss=((RANDOM%2))
-	case $toss in 
+
+function tossAndAssignSymbol(){
+	toss=$((RANDOM%2))
+	case $toss in
 		1)
-			player=X ;;
-		2)
-			player=O ;;
+			player=x ;;
+		0)
+			player=$computerPlayer ;;
 	esac
+	echo $player
 }
+
 function displayBord(){
 	for (( i=0; i<$ROW; i++ ))
 	do
 			echo "---+---+---+"
 		for (( j=0; j<$COLUMN; j++ ))
 		do
-			echo -e " ${bord[$i,$j]} |\c"
+			printf " ${bord[$i,$j]} |"
    	done
 		echo
 	done
 	echo "---+---+---+"
-
 }
+
+function checkWin(){
+   win=0
+   for (( i=0; i<$ROW; i++ ))
+   do
+      if [[ ${bord[$i,$((i-i))]} == $1 && ${bord[$i,$((i+1-i))]} == $1 && ${bord[$i,$((i+2-i))]} == $1 ]]
+      then
+         win=1
+      elif [[ ${bord[$((i-i)),$i]} == $1 && ${bord[$((i+1-i)),$i]} == $1 &&  ${bord[$((i+2-i)),$i]} == $1 ]]
+      then
+         win=1
+      elif [[ $i -eq 0 && ${bord[$i,$i]} == $1 && ${bord[$((i+1)),$((i+1))]} == $1 && ${bord[$((i+1)),$((i+1))]} == $1 ]]
+      then
+         win=1
+      elif [[ ${bord[0,2]} == $1 && ${bord[1,1]} == $1 && ${bord[2,0]} == $1 ]]
+      then
+         win=1
+	   fi
+	done
+}
+
+
 function changePlayer(){
 	if [[ $player == x ]]
 	then
@@ -43,6 +69,43 @@ function changePlayer(){
 		player=x
 	fi
 }
+
+function checkICanWinThenPlay(){
+	flag=1
+	for (( r=0; r<$ROW; r++ ))
+	do
+   	for (( c=0; c<$COLUMN; c++ ))
+   	do
+      	if [[ ${bord[$r,$c]} == " " ]]
+      	then
+         	bord[$r,$c]=$1
+         	checkWin $1
+         	if [ $win -eq 0 ]
+         	then
+            	 bord[$r,$c]=" "
+         	elif [[ $win -eq 1 && ${bord[$r,$c]} == $player ]]
+         	then
+            	displayBord
+            	echo "$player win "
+            exit
+ 				elif [ $win -eq 1 ]
+         	then
+            	bord[$r,$c]=$player
+            	displayBord
+            	win=0
+            	flag=0
+            	((countOfMoves++))
+            	break
+         	fi
+      	fi
+   	done
+	if [ $flag -eq 0 ]
+	then
+   	break
+	fi 
+	done
+}
+
 function checkEmpty(){
 	if [[ ${bord[$2,$3]} == " " ]]
 	then
@@ -50,54 +113,12 @@ function checkEmpty(){
 		displayBord
 		countOfMoves=$((countOfMoves+1))
 		changePlayer $1
+		checkWin $1
 	else
-		echo "this position is not empty"
-	fi
-}
-function checkWin(){
-
-	for (( i=0; i<$ROW; i++ ))
-	do
-		if [[ ${bord[$i,$((i-i))]} == $1 && ${bord[$i,$((i+1-i))]} == $1 && ${bord[$i,$((i+2-i))]} == $1 ]]
-		then
-	 		echo "win!"
-    		exit
-		fi
-		if [[ ${bord[$((i-i)),$i]} == $1 && ${bord[$((i+1-i)),$i]} == $1 && ${bord[$((i+2-i)),$i]} == $1 ]]
-		then
-	 		echo "win!"
-    		exit
-		fi
-	done
-	if [[ ${bord[0,0]} == $1 && ${bord[1,1]} == $1 && ${bord[2,2]} == $1 ]]
-	then
-		echo "win!"
-		exit
-	fi
-	if [[ ${bord[0,2]} == $1 && ${bord[1,1]} == $1 && ${bord[2,0]} == $1 ]]
-	then 
-		echo "win!"
-		exit
+		echo "invalid place"
 	fi
 }
 
-function checkICanWinThenPlay(){
-for (( r=0; r<$ROW; r++ ))
-do
-   for (( c=0; c<$COLUMN; c++ ))
-   do
-      if [[ ${bord[$r,$c]} == " " ]]
-		then
-         bord[$r,$c]=$1
-         checkWin $1
-         if [[ $win -eq 0 ]]
-			then 
-             bord[$r,$c]=" "
-			fi
-		fi
-   done
-done
-}
 resetingBord
 tossAndAssignSymbol
 displayBord
@@ -108,11 +129,26 @@ do
 		read -p "enter row position" rowPosition
 		read -p "enter column position" columnPosition
 		checkEmpty $player $rowPosition $columnPosition
-		checkWin $player
-	else
-		position=$((RANDOM%9))
-		rowPosition=$((position/3))
-		columnPosition=$((position%3))
-      checkEmpty $player $rowPosition $columnPosition
-	fi
+	 else
+		otherPlayer="x"
+		checkICanWinThenPlay $player
+		checkICanWinThenPlay $otherPlayer
+		if [ $flag -eq 1 ]
+		then
+			position=$((RANDOM%9))
+			rowPosition=$((position/3))
+			columnPosition=$((position%3))
+			checkEmpty $player $rowPosition $columnPosition
+		else
+			changePlayer $otherPlayer
+		fi
+	 fi
+if [[ $win == 1 ]]
+then
+	break
+fi
 done
+if [[ $win == 0 ]]
+then
+echo "match tie"
+fi
