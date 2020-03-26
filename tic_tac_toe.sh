f0413b6134d46c04e2435b856867032df819cf95
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-26 15:48:37 +0530

    Added:-reseting bord

--- tic_tac_toe.sh
+++ tic_tac_toe.sh
@@ -1,1 +1,15 @@
-echo "welcome to tic tac toe"
+#!/bin/bash -x
+echo "welcome to tic_tac_toe"
+declare -A bord
+ROW=3
+COLUMN=3
+function reseting(){
+for (( i=0; i<$ROW; i++ ))
+do
+	for (( j=0; j<$COLUMN; j++ ))
+	do
+		bord[$i,$j]=" "
+	done
+done
+}
+reseting
