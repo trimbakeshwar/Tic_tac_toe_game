554805ff12d4937c8a9b4e06be17cfd2a71578e3
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-26 16:16:27 +0530

    Added:-check win between wo player

--- tic_tac_toe.sh
+++ tic_tac_toe.sh
@@ -35,3 +35,66 @@ function displayBord(){
 	echo "---+---+---+"
 
 }
+function changePlayer(){
+	if [[ $player == x ]]
+	then
+		player=o
+	else
+		player=x
+	fi
+}
+function checkEmpty(){
+	if [[ ${bord[$2,$3]} == " " ]]
+	then
+		bord[$2,$3]=$1
+		displayBord
+		countOfMoves=$((countOfMoves+1))
+		changePlayer $1
+	else
+		echo "this position is not empty"
+	fi
+}
+function checkWin(){
+
+	for (( i=0; i<$ROW; i++ ))
+	do
+		if [[ ${bord[$i,$((i-i))]} == $1 && ${bord[$i,$((i+1-i))]} == $1 && ${bord[$i,$((i+2-i))]} == $1 ]]
+		then
+	 		echo "win!"
+    		exit
+		fi
+		if [[ ${bord[$((i-i)),$i]} == $1 && ${bord[$((i+1-i)),$i]} == $1 && ${bord[$((i+2-i)),$i]} == $1 ]]
+		then
+	 		echo "win!"
+    		exit
+		fi
+	done
+	if [[ ${bord[0,0]} == $1 && ${bord[1,1]} == $1 && ${bord[2,2]} == $1 ]]
+	then
+		echo "win!"
+		exit
+	fi
+	if [[ ${bord[0,2]} == $1 && ${bord[1,1]} == $1 && ${bord[2,0]} == $1 ]]
+	then 
+		echo "win!"
+		exit
+	fi
+}
+resetingBord
+tossAndAssignSymbol
+displayBord
+while [ $countOfMoves -lt $totalNumberOfMoves ]
+do
+	if [[ $player == x ]]
+	then
+		read -p "enter row position" rowPosition
+		read -p "enter column position" columnPosition
+		checkEmpty $player $rowPosition $columnPosition
+		checkWin $player
+	else
+		read -p "enter row position" rowPosition
+		read -p "enter column position" columnPosition
+		checkEmpty $player $rowPosition $columnPosition
+		checkWin $player
+	fi
+done
